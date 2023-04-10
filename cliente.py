28f20a97021b194870c4176ea20f0bd6b3c67bb5
Mateo Lopez <m.lopez23@uniandes.edu.co>
2023-04-10 14:39:23 -0500

    correccion carpetas

---
+++ cliente.py
@@ -0,0 +1,101 @@
+from datetime import datetime
+import os
+import socket
+import threading
+import time
+
+MAX_PACKET_SIZE = 2048
+lock = threading.Lock()
+
+# function to handle logs
+def log(cliente, tiempo, nombreLog, tamanio_final, exito):
+    texto = ["Conexión del cliente {} \n Tiempo de transferencia: {}s \n Tamaño final: {}MB \n Éxito: {}\n".format(cliente, tiempo, tamanio_final, exito) ]
+    lock.acquire()
+    with open("Logs/" + nombreLog, "a") as f:
+        f.writelines(texto)
+        f.close()
+    lock.release() 
+
+def client(filename, request, nombre_log, num_cliente):
+    # set up the UDP client socket
+    udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_client_socket.settimeout(10)
+    server_address = ('192.168.89.131', 12345)
+    udp_client_socket.sendto(request.encode(), server_address)
+
+    # Open the file to write the received data
+    if not os.path.exists("Archivos Recibidos"):
+        os.makedirs("Archivos Recibidos")
+    file = open("Archivos Recibidos\ ".strip() + filename, 'wb')
+
+    # Loop over the incoming data and write it to the file
+    start = time.time()
+    success = True
+    while True:
+        try:
+            data, server_address = udp_client_socket.recvfrom(MAX_PACKET_SIZE)
+        except socket.timeout:
+            #print("Connection timed out for {}".format(filename))
+            success = False
+            file.close()
+            break
+        file.write(data)
+        if not data or len(data) < MAX_PACKET_SIZE:
+            file.close()
+            break
+    end = time.time()
+    tiempo = end - start
+    print('File received from {}:{}'.format(server_address[0], server_address[1]))
+    tamanio_final = int(os.path.getsize("Archivos Recibidos\ ".strip() + filename))/1000000
+    log(num_cliente, round(tiempo, 2), nombre_log, tamanio_final, success)
+    # close the UDP client socket
+    udp_client_socket.close()
+
+
+def main():
+    print("Bienvenido al cliente de archivos \n Escoja el tamaño de archivo que desea enviar: \n 1. 100MB \n 2. 250MB")
+    opcion = input("Ingrese el numero de la opcion: ")
+    if opcion == "1":
+        request = '100MB'
+        filename = 'prueba100.txt'
+    elif opcion == "2":
+        request = '250MB'
+        filename = 'prueba250.txt'
+    else:
+        print("Opcion no valida")
+        exit()
+
+    numClientes = int(input("Ingrese el numero de clientes: "))
+
+    # set up the TCP client socket and connect to the server
+    tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('192.168.89.131', 12345)
+    tcp_client_socket.connect(server_address)
+
+    # send the request over the TCP socket
+    tcp_client_socket.send((request + " " + str(numClientes)).encode())
+
+    # close the TCP client socket
+    tcp_client_socket.close()
+
+
+    nombreLog = datetime.now().strftime("%d-%m-%Y_%H-%M-%S") + ".txt"
+    texto = ["Archivo recibido: " + filename + "\n"]
+    if not os.path.exists("Logs"):
+        os.makedirs("Logs")
+    with open("Logs/"+nombreLog, "w") as f:
+        f.writelines(texto)
+        f.close()
+        
+    for i in range(1, numClientes+1):
+        nombre = f"Cliente{i}-Prueba-{numClientes}.txt"
+        thread = threading.Thread(target=client, args=(nombre, request, nombreLog, i))
+        thread.start()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ servidor.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+import socket
+import os
+import threading
+import time
+import hashlib
+
+MAX_PACKET_SIZE = 2048
+lock = threading.Lock()
+
+# function to handle logs
+def log(cliente, tiempo, nombreLog):
+    texto = ["Conexión del cliente {} \n Tiempo de transferencia: {}s\n".format(cliente, tiempo) ]
+    lock.acquire()
+    with open("Logs/" + nombreLog, "a") as f:
+        f.writelines(texto)
+        f.close()
+    lock.release()  
+
+# function to handle client requests
+def handle_client_request(client_address, filename, num_cliente, nombreLog):
+    # read the file data from the file
+    file = open(filename, 'rb')
+    start = time.time()
+    while True:
+        # Read the file data in chunks of MAX_PACKET_SIZE
+        file_data = file.read(MAX_PACKET_SIZE)
+
+        # If there is no more data to read, break the loop
+        if not file_data:
+            break
+
+        # send the data packet to the client
+        sent = 0
+        while sent < len(file_data):
+            packet = file_data[sent:sent + MAX_PACKET_SIZE]
+            server_socket.sendto(packet, client_address)
+            sent += MAX_PACKET_SIZE
+    end = time.time()
+    tiempo = end - start
+    log(num_cliente, round(tiempo, 2), nombreLog)
+
+    print('File sent to {}:{}'.format(client_address[0], client_address[1]))
+    file.close()
+
+# main function
+def main():
+    # set up the TCP server socket
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_server_address = ('127.0.0.1', 12345)
+    tcp_server_socket.bind(tcp_server_address)
+    tcp_server_socket.listen()
+
+    # listen for incoming TCP connections to receive the initial request
+    print('TCP server is listening on {}:{}'.format(tcp_server_address[0], tcp_server_address[1]))
+    tcp_client_socket, tcp_client_address = tcp_server_socket.accept()
+
+    # receive the initial request from the client
+    data = tcp_client_socket.recv(MAX_PACKET_SIZE)
+    if '100MB' in data.decode():
+        filename = 'prueba100.txt'
+        tcp_client_socket.send('File exists'.encode())
+    elif '250MB' in data.decode():
+        filename = 'prueba250.txt'
+        tcp_client_socket.send('File exists'.encode())
+    else:
+        tcp_client_socket.close()
+        return
+
+    numClientes = int(data.decode().split()[1])
+    # close the TCP socket for the initial request
+    tcp_client_socket.close()
+
+    # set up the UDP server socket
+    global server_socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # set up the server address and port
+    server_address = ('127.0.0.1', 12345)
+    server_socket.bind(server_address)
+
+    # listen for incoming UDP requests
+    print('UDP server is listening on {}:{}'.format(server_address[0], server_address[1]))
+
+    nombreLog = datetime.now().strftime("%d-%m-%Y_%H-%M-%S") + ".txt"
+    texto = ["Archivo enviado: " + filename + "\n", "Tamaño del archivo: " + str(int((os.path.getsize(filename)))/1000000) + "MB\n"]
+    if not os.path.exists("Logs"):
+        os.makedirs("Logs")
+    with open("Logs/"+nombreLog, "w") as f:
+        f.writelines(texto)
+        f.close()
+
+    threads = []
+    for i in range(1, numClientes+1):
+        # accept an incoming request
+        data, client_address = server_socket.recvfrom(MAX_PACKET_SIZE)
+        # create a thread to handle the client request
+        thread = threading.Thread(target=handle_client_request, args=(client_address, filename, i, nombreLog))
+        thread.start()
+        threads.append(thread)
+
+    # wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
+    # close the server socket
+    server_socket.close()
+
+if __name__ == '__main__':
+    main()
